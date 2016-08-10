c8da9d2283d075af0cb3df1bcd0294278c45e64e
dapayne <damon.payne@perficient.com>
2016-08-10 14:21:37 -0500

    Adding a comment to trigger a build to test GitHub push to Jenkins.

--- src/main/java/com/mspairport/utils/GenericHttpClient.java
+++ src/main/java/com/mspairport/utils/GenericHttpClient.java
@@ -7,7 +7,7 @@ public class GenericHttpClient {
 
 	public static void main(String[] args) throws Exception {
 
-		
+		// Adding another comment to test out Continuous Integration
 		Map<String,String> inputMap = new HashMap<String,String>();
 		
 		for (int i=0; i < args.length; i++) {
